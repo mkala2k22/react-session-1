602e57533845466bd3609b55942278f62cd1ccb7
mkala2k22 <109620709+mkala2k22@users.noreply.github.com>
2023-08-11 22:49:33 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="container">
+      <div className="card w-75">
+        <div className="card-body">
+        <h5 className="card-title">My first react applicaiton</h5>
+        <p className="card-text">Created the application with the help of create react app and bootstrap.</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
